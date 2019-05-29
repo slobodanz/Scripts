d8825d9c586076e0e0e922c8a6cf078403eb7d1e
slobodanz <slobodan@atomia.com>
2019-05-29 15:20:41 +0200

    Update le_auth.sh

--- le_auth.sh
+++ le_auth.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-CERTBOT_DOMAIN='atm0357.telia.atomia.se'
-CERTBOT_VALIDATION='143132423412'
-CERTBOT_TOKEN='afasdfasdf'
-
-
 WWW="www."
 MAPS="/storage/configuration/maps"
 DOMAIN=${CERTBOT_DOMAIN#$WWW}
@@ -14,9 +9,10 @@ LAST=`echo -n $USER | tail -c 2`
 ROOTDIR="/storage/content/$LAST/$USER/$DOMAIN/public_html"
 
 if [ -d "$ROOTDIR" ]; then
-  echo "mkdir -p $ROOTDIR/.well-known/acme-challenge"
-  echo "echo $CERTBOT_VALIDATION > $ROOTDIR/.well-known/acme-challenge/$CERTBOT_TOKEN"
-  echo "chown $USER:apache -R $ROOTDIR/.well-known"
+   mkdir -p $ROOTDIR/.well-known/acme-challenge
+   echo $CERTBOT_VALIDATION > $ROOTDIR/.well-known/acme-challenge/$CERTBOT_TOKEN
+   chown $USER:apache -R $ROOTDIR/.well-known
 else
+  echo "Dir $ROOTDIR does not exsits"
   exit 1
 fi
